9be78ad35315c21a85a24d04a8a0d9005e27d379
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-09-23 17:20:11 +0100

    `Refactored IRepository to abstract class Repository and updated TransactionRepository to extend it`

--- src/domain/interfaces/Repository.java
+++ src/domain/interfaces/Repository.java
@@ -1,12 +1,18 @@
 package domain.interfaces;
+import java.sql.Connection;
 import  java.util.List;
 import java.util.UUID;
+import domain.repository.dataBaseConnection;
 
-public interface IRepository<T> {
-    void Save(T Object);
-    T FindById(UUID Id);
-    List<T> FindAll();
-    void Delete(UUID Id);
-    
+public abstract class  Repository<T> {
+    protected String Delect = "SELECT * FROM";
+    protected String Delete= "DELETE FROM";
+    protected String Update= "UPDATE ";
+    protected String Insert= "INSERT INTO ";
+    protected final Connection Instance = dataBaseConnection.getInstence();
 
+    public abstract void Save(T Object);
+    public abstract T FindById(UUID Id);
+    public abstract List<T> FindAll();
+    public abstract void Delete(UUID Id);
 }
--- src/domain/repository/TransactionRepository.java
+++ src/domain/repository/TransactionRepository.java
@@ -1,5 +1,34 @@
 package domain.repository;
 
-public class TransactionRepository {
+import domain.entities.Transaction;
+import domain.interfaces.Repository;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public class TransactionRepository extends Repository<Transaction> {
+
+    @Override
+    public void Save(Transaction Object) {
+        try {
+
+        }catch (Exception e){}
+
+    }
+
+    @Override
+    public Transaction FindById(UUID Id) {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> FindAll() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void Delete(UUID Id) {
+
+    }
 }
