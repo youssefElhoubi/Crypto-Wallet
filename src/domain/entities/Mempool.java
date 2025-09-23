0b67d60d0bc2d5db1a0c6e83dd437c118a76f8f3
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-09-23 22:54:58 +0100

    Add Mempool class with pending transactions management and blockchain attributes

--- src/domain/entities/Mempool.java
+++ src/domain/entities/Mempool.java
@@ -1,4 +1,44 @@
 package domain.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mempool {
+    // List of pending transactions
+    private List<Transaction> pendingTransactions = new ArrayList<>();
+
+    // Optional: maximum capacity of mempool (simulate network congestion)
+    private int maxCapacity;
+
+    // Optional: name/id of the blockchain (Bitcoin, Ethereum)
+    private String blockchainType;
+
+    public Mempool(String blockchainType, int maxCapacity) {
+        this.blockchainType = blockchainType;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public List<Transaction> getPendingTransactions() {
+        return pendingTransactions;
+    }
+
+    public void setPendingTransactions(List<Transaction> pendingTransactions) {
+        this.pendingTransactions = pendingTransactions;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public String getBlockchainType() {
+        return blockchainType;
+    }
+
+    public void setBlockchainType(String blockchainType) {
+        this.blockchainType = blockchainType;
+    }
 }
