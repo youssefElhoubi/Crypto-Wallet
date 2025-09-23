23d66773f4a4b52b817ffce998c91f8f71f480d8
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-09-23 14:04:36 +0100

    compiled all the files to bite code in the out directory

--- src/domain/repository/dataBaseConnection.java
+++ src/domain/repository/dataBaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dataBaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
+    private static final String URL = "jdbc:mysql://localhost:3306/crypto_wallet";
     private static final String USER = "root";
     private static final String PASSWORD = "1234";
     private static Connection Instance = null;
